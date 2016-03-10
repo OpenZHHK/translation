cd1dbc1f02067258bc4a5f4079ec805f5b3a14fc
Ankur Singh <ankur13019@iiitd.ac.in>
2016-03-10 23:48:23 +0530

    file generation

--- openzhhk/api.py
+++ openzhhk/api.py
@@ -1,6 +1,8 @@
 from flask_restful import reqparse, abort, Api, Resource
-from flask import jsonify, Blueprint, request
+from flask import jsonify, Blueprint, request, send_file
 from openzhhk.models import Word
+from cStringIO import StringIO
+
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
@@ -20,75 +22,74 @@ file_parser = reqparse.RequestParser()
 
 
 def get_word(slug):
-    return Word.objects.get_or_404(id=slug)
+	return Word.objects.get_or_404(id=slug)
 
 
 def parse_file(file_obj):
-    # TODO: Parse File
-    objects = file_obj
-    return objects
-
-
-def generate_file(objects):
-    # TODO: Generate File
-    file_obj = objects
-    return file_obj
-
+	# TODO: Parse File
+	objects = file_obj
+	return objects
 
 class Words(Resource):
-    def get(self, slug):
-        obj = get_word(slug)
-        return jsonify({'status': 'ok', 'word': obj})
+	def get(self, slug):
+		obj = get_word(slug)
+		return jsonify({'status': 'ok', 'word': obj})
 
-    def delete(self, slug):
-        obj = get_word(slug)
-        obj.soft_delete()
-        return jsonify({'status': 'ok'})
+	def delete(self, slug):
+		obj = get_word(slug)
+		obj.soft_delete()
+		return jsonify({'status': 'ok'})
 
-    def put(self, slug):
-        obj = get_word(slug)
-        args = form_parser.parse_args()
-        args["lastip"] = request.remote_addr
-        obj.update(**args)
-        obj.reload()
-        return jsonify({'status': 'ok', 'word': obj})
+	def put(self, slug):
+		obj = get_word(slug)
+		args = form_parser.parse_args()
+		args["lastip"] = request.remote_addr
+		obj.update(**args)
+		obj.reload()
+		return jsonify({'status': 'ok', 'word': obj})
 
 
 class WordList(Resource):
-    def get(self):
-        args = parser.parse_args()
-        print args
-        words = Word.get_paginated(**args)
-        print words
-        return jsonify({'status': 'ok', 'words': words.items, 'count': words.per_page, 'page': words.page,
-                        'pages': words.pages, 'has_next': words.has_next})
+	def get(self):
+		args = parser.parse_args()
+		words = Word.get_paginated(**args)
+		return jsonify({'status': 'ok', 'words': words.items, 'count': words.per_page, 'page': words.page,
+		                'pages': words.pages, 'has_next': words.has_next})
 
-    def post(self):
-        args = form_parser.parse_args()
-        args["lastip"] = request.remote_addr
-        args["originalip"] = request.remote_addr
-        args["slug"] = args["inputtext"]
-        obj = Word(**args)
-        obj.save()
-        return jsonify({'status': 'ok', 'word': obj})
+	def post(self):
+		args = form_parser.parse_args()
+		args["lastip"] = request.remote_addr
+		args["originalip"] = request.remote_addr
+		args["slug"] = args["inputtext"]
+		obj = Word(**args)
+		obj.save()
+		return jsonify({'status': 'ok', 'word': obj})
 
 
 class WordFile(Resource):
-    def get(self):
-        words = Word.active_objects.all()
-        file = generate_file(words)
-        # TODO: Return this file
-        return jsonify({'status': 'ok', 'file': file})
+	def get(self):
+		words = Word.active_objects.all()
+		lines = ""
+		for obj in words:
+			lines += "i=%s,t=%s,f=%s,ff=%s\n" % (obj.inputtext, obj.translation, obj.frequency, obj.flags)
+		print lines
+		sio = StringIO()
+		sio.write(lines)
+		sio.seek(0)
+		return send_file(sio,
+		                 attachment_filename="words.txt",
+		                 as_attachment=True)
 
-    def post(self):
-        # TODO: Get the file object
-        args = file_parser.parse_args()
-        file = args
-        # TODO: Get objects from file
-        objs = parse_file(file)
-        # TODO: Insert objects into db
-        Word(**objs).save()
-        return jsonify({'status': 'ok'})
+	def post(self):
+		# TODO: Get the file object
+		args = file_parser.parse_args()
+		file = args
+		# TODO: Get objects from file
+		objs = parse_file(file)
+		# TODO: Insert objects into db
+		Word(**objs).save()
+		return jsonify({'status': 'ok'})
+
 
 api.add_resource(WordList, '/api/v1/words')
 api.add_resource(WordFile, '/api/v1/words_file')
