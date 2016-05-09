8d987f6fec67cb80183405d91a81bac58d8590ad
Joseph Lam <me@lamjoseph.com>
2016-05-09 11:36:21 -0400

    Replacing %20 in searh string to space instead to allow for proper searcing

--- openzhhk/views.py
+++ openzhhk/views.py
@@ -20,7 +20,6 @@ class ListView(MethodView):
     def get(self):
         args = parser.parse_args()
         sw = 0
-		query = args["q"].strip().lower()
         if args["q"] == "":
             return render_template('list.html', q=args["q"], sw=sw)
         if args["singleword"] in true_values:
